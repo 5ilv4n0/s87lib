d9e57bd9d524a376dcf80f91cffe6b2242d5f6f5
Silvano Wegener <silvano.wegener@googlemail.com>
2012-09-06 15:02:35 +0200

    Help-Klasse implementiert

--- basic.py
+++ basic.py
@@ -21,4 +21,64 @@
 #  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 #  MA 02110-1301, USA.
 #  
-#  
+# 
+
+
+import sys, os
+
+
+def makeString(*strings, **keyWordArgs):
+	try:
+		joiner = keyWordArgs['joiner']
+	except KeyError:
+		joiner = ' '
+	return joiner.join(strings)
+
+
+class Help(object):
+	def __init__(self):
+		self.parameters = {}
+		self.addParameter('--help','this info.')
+		self.description = 'no description available'
+
+
+	def addParameter(self, parameter, description='no description available.'):
+		if parameter == '' or self.existsParameter(parameter):
+			return False
+		self.parameters[parameter] = description
+
+
+	def addDescription(self, description):
+		self.description = description
+
+
+	def existsParameter(self, parameter):
+		try:
+			check =  self.parameters[parameter]
+			return True
+		except KeyError:
+			return False
+
+
+	def printHelp(self):
+		topLine = os.path.split(sys.argv[0])[1] + ' - help and description.'
+		print topLine
+		print '-'*len(topLine)
+		print 'Description:\n ' + self.description +'\n'
+		print 'Parameters:'
+		parametersMaxLength = self.longestParameterLength()
+		for parameter in self.parameters.keys():
+			infoLine = makeString(' ',parameter.ljust(parametersMaxLength, ' '),'|',self.parameters[parameter])
+			print infoLine
+		print '-'*len(topLine)
+
+
+	def longestParameterLength(self):
+		parameterLength = 0
+		for parameter in self.parameters.keys():
+			if len(parameter) > parameterLength:
+				parameterLength = len(parameter)
+		return parameterLength
+
+
+
